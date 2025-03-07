b1c79d3bc4441f4b191747f6f87eebe9793eac09
ErikaAX08 <eamastalxochimitl@gmail.com>
2025-03-06 19:37:23 -0600

    router created

---
+++ app/core/Router.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Core;
+
+class Router {
+    public function bootstrap() {
+        define('BASE_PATH', __DIR__ . '/../../');
+        define('BASE_VIEWS', BASE_PATH . 'app/views/');
+        define('BASE_TEMPLATES', BASE_PATH . 'app/templates/');
+        define('BASE_ASSETS', '/Hermonia/assets/');
+
+        $route = isset($_GET['route']) ? $_GET['route'] : 'home';
+
+        $controller = ucfirst($route) . 'Controller';
+        $controller = "App\\Controllers\\" . $controller;
+
+        if (class_exists($controller)) {
+            $controller = new $controller;
+            $controller->index();
+        } else {
+            echo "page not found";
+        }
+    }
+
+}
