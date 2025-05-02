1c91269d4ddc1e397fefeeb21a66e67fdc77a815
Axel Tapia <axurmen214@gmail.com>
2025-05-01 21:03:45 -0600

    Se omite ir a la vista player, en su lugar el usuario debe de iniciar sesión again

--- app/views/logged_view.php
+++ app/views/logged_view.php
@@ -48,6 +48,20 @@ include BASE_TEMPLATES . 'header.php';
         </div>
     </main>
 
-    <script src="<?php echo BASE_ASSETS; ?>js/logged.js"></script>
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+        const buttonNavigateToPlayer = document.getElementById("navigate_to_player");
+
+        if (buttonNavigateToPlayer) {
+            buttonNavigateToPlayer.addEventListener("click", function() {
+                const currentOrigin = window.location.origin; // http://localhost
+                const pathParts = window.location.pathname.split('/');
+                const projectFolder = pathParts[1];
+
+                window.location.href = `${currentOrigin}/${projectFolder}/player`;
+            });
+        }
+    });
+    </script>
 
 </body>
